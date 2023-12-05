b98bb282d27ea1427c0787f38caf1ab8786e6e39
hameee <113837605+qwerty00ui88@users.noreply.github.com>
2023-12-05 12:29:59 +0900

    section01-quiz04

---
+++ section01/quiz04/Main.java
@@ -0,0 +1,46 @@
+package section01.quiz04;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	public ArrayList<String> solution(int n, String[] str) {
+		ArrayList<String> answer = new ArrayList<>();
+//		// 방법1
+//		for(String x : str) {
+//			String tmp = new StringBuilder(x).reverse().toString();
+//			answer.add(tmp);
+//		}
+		
+		// 방법2
+		for(String x : str) {
+			char[] s = x.toCharArray();
+			int lt = 0, rt = x.length() - 1;
+			while(lt < rt) {
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = tmp;
+				lt++;
+				rt--;
+			}
+			String tmp = String.valueOf(s);
+			answer.add(tmp);
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		String[] str = new String[n];
+		for(int i = 0; i < n; i++) {
+			str[i] = kb.next();
+		}
+		for(String x : T.solution(n, str)) {
+			System.out.println(x);
+		}
+	}
+
+}
