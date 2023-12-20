fd8be43ef1570d2e7b0e2e12f12e126fdceab03f
hameee <sohui.dev@gmail.com>
2023-12-20 11:43:27 +0900

    section02-quiz02

---
+++ section02/quiz02/Main.java
@@ -0,0 +1,29 @@
+package section02.quiz02;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public int solution(int n, int[] arr) {
+		int answer = 1, max = arr[0];
+		for(int i = 1; i < n; i++) {
+			if(arr[i] > max) {
+				answer++;
+				max = arr[i];
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		System.out.print(T.solution(n, arr));
+	}
+
+}
