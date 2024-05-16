4f6e08bca421020588160d4d667b46278a3d36e2
hameee <sohui.dev@gmail.com>
2024-05-16 12:53:56 +0900

    section03-quiz05

--- section03/quiz05/Main.java
+++ section03/quiz05/Main.java
@@ -5,17 +5,28 @@ import java.util.Scanner;
 public class Main {
 
 	public int solution(int n) {
-		int answer = 0, sum = 0, lt = 0;
-		int m = n / 2 + 1;
-		int[] arr = new int[m];
-		for(int i = 0; i < m; i++) arr[i] = i + 1;
-		for(int rt = 0; rt < m; rt++) {
-			sum += arr[rt];
-			if(sum == n) answer++;
-			while(sum >= n) {
-				sum -= arr[lt++];
-				if(sum == n) answer++;
-			}
+		// 방법1		
+//		int answer = 0, sum = 0, lt = 0;
+//		int m = n / 2 + 1;
+//		int[] arr = new int[m];
+//		for(int i = 0; i < m; i++) arr[i] = i + 1;
+//		for(int rt = 0; rt < m; rt++) {
+//			sum += arr[rt];
+//			if(sum == n) answer++;
+//			while(sum >= n) {
+//				sum -= arr[lt++];
+//				if(sum == n) answer++;
+//			}
+//		}
+//		return answer;
+		
+		// 방법2
+		int answer = 0, cnt = 1;
+		n--;
+		while(n > 0) {
+			cnt++;
+			n = n - cnt;
+			if(n % cnt == 0) answer++;
 		}
 		return answer;
 	}
