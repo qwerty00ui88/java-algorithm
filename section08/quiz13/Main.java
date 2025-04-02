f0d01be3728ccfc631cc3115bbda6174cd5c3c23
sohui.dev <113837605+qwerty00ui88@users.noreply.github.com>
2025-04-02 18:10:46 +0900

    section08-quiz13

--- section08/quiz13/Main.java
+++ section08/quiz13/Main.java
@@ -3,42 +3,42 @@ package section08.quiz13;
 import java.util.Scanner;
 
 class Main {
-	static int answer=0, n;
-	static int[] dx={-1, -1, 0, 1, 1, 1, 0, -1};
-	static int[] dy={0, 1, 1, 1, 0, -1, -1, -1};
-	public void DFS(int x, int y, int[][] board){
-		for(int i=0; i<8; i++){
-			int nx=x+dx[i];
-			int ny=y+dy[i];
-			if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1){
-				board[nx][ny]=0;
-				DFS(nx, ny, board);
-			}
-		}	
-	}
-	public void solution(int[][] board){
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				if(board[i][j]==1){
-					answer++;
-					board[i][j]=0;
-					DFS(i, j, board);
-				}
-			}
-		}	
-	}
+	static int n, answer;
+	static int[][] board;
+	static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-	public static void main(String[] args){
-		Main T = new Main();
-		Scanner kb = new Scanner(System.in);
-		n=kb.nextInt();
-		int[][] arr=new int[n][n];
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				arr[i][j]=kb.nextInt();
+	public void DFS(int x, int y) {
+		for (int i = 0; i < 8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+				board[nx][ny] = 0;
+				DFS(nx, ny);
 			}
 		}
-		T.solution(arr);
-		System.out.println(answer);
 	}
+
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		n = kb.nextInt();
+		board = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				board[i][j] = kb.nextInt();
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == 1) {
+					answer++;
+					board[i][j] = 0;
+					T.DFS(i, j);
+				}
+			}
+		}
+		System.out.print(answer);
+	}
+
 }
